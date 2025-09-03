38b670b25e567942ecf6f196aee216bda706131c
Hirun-Kasuntha <manjulaliyanage08@gmail.com>
2025-09-03 10:44:21 +0530

    test

---
+++ src/lk/avinam/session/Session.java
@@ -0,0 +1,29 @@
+
+package lk.avinam.session;
+
+public class Session {
+
+    private static Session instance;
+    private String username; 
+
+    private Session() {
+    }
+    public static Session getInstance() {
+        if (instance == null) {
+            instance = new Session();
+        }
+        return instance;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void clear() {
+        username = null;
+    }
+}
