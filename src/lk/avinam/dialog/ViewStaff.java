2af79b324b533acc64dc784d4f60f87f5d3a07fa
abesi <abesi@MSI>
2025-09-08 22:39:44 +0530

    Merge origin/main into main
    # Conflicts:
    #	src/lk/avinam/dialog/AddStaffeUpdate.form
    #	src/lk/avinam/dialog/AddStaffeUpdate.java

--- src/lk/avinam/dialog/ViewStaff.java
+++ src/lk/avinam/dialog/ViewStaff.java
@@ -241,7 +241,7 @@ public class ViewStaff extends javax.swing.JDialog {
                     .addComponent(availableBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(18, Short.MAX_VALUE))
+                .addContainerGap(36, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -252,7 +252,7 @@ public class ViewStaff extends javax.swing.JDialog {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
